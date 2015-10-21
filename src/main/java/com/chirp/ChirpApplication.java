1b289568bee16b597a280437f707c5e433a10127
OJT-Ralen <OJT-Ralen@Pittidae.local>
2015-10-21 13:24:03 +0800

    project

---
+++ src/main/java/com/chirp/ChirpApplication.java
@@ -0,0 +1,12 @@
+package com.chirp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChirpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChirpApplication.class, args);
+    }
+}
